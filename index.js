5bde9ea1b3b3d2b1b0ef35dbeb0872f6ceff988e
Simon <simon@Simons-MacBook-Pro.local>
2025-12-29 16:29:01 +0800

    chore: test nodejs 12 debugger with nodemon process in docker container

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const PORT = 8081;
+
+app.get('/health', (req, res) => {
+    // const temp = req.body?.hello?.world;
+
+    // if (temp) {
+    //     console.log('hello world')
+    // }
+
+  console.log('hello express');
+  res.json({ status: 'ok' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
