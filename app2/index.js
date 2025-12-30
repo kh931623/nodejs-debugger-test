42e5e5267a9b62e96b588835df8ba4f1009acb90
Simon <simon@Simons-MacBook-Pro.local>
2025-12-30 14:20:30 +0800

    chore: setting up app2

---
+++ app2/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const PORT = 8081;
+
+app.use(express.json());
+
+app.get('/health', (req, res) => {
+  const temp = req.body?.hello?.world;
+
+  if (temp) {
+    console.log('hello world')
+  }
+
+  console.log('hello express');
+  res.json({
+    status: 'ok',
+    msg: 'app2 is running'
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
